04c32bd5bd20019308a1ba1eb65f29966990fc2a
Vishal Aitha <aithavis@amazon.com>
2024-04-23 20:44:10 +0530

    Second commit

--- index.js
+++ index.js
@@ -4,4 +4,14 @@ const app = new express()
 
 app.listen(5001, () => console.log("Api running on port 5001"))
 
-app.get('/', (req, res) => res.json('My Api is running 😎'))
+app.get('/', (req, res) => {
+    res.json('My Api is running 😎');
+    res.send({ "Response": "My Api is running 😎" });
+
+})
+const data = require('/path/to/data.json')
+
+app.get('/search', function (req, res) {
+    res.header("Content-Type", 'application/json');
+    res.send(JSON.stringify(data));
+})
