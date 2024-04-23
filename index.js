38f5fd9cd1d1f1741113eeb1b5a2a19e9c71807d
Vishal Aitha <aithavis@amazon.com>
2024-04-23 23:37:54 +0530

    Working Data

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
-import express, { json } from 'express'
+import express from 'express';
+import bodyParser from 'body-parser';
+import fs from 'fs';
 
-const app = new express()
+const app = express();
+const port = 3013;
 
-app.listen(5003, () => console.log("Api running on port 5003"))
+// Parse incoming request bodies in JSON format
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.json('My Api is running 😎');
-    res.send({ "Response": "My Api is running 😎" });
+// Define a route to handle incoming POST requests to /api
+app.post('/customers', (req, res) => {
+    // Read the JSON array from data.json file
+    fs.readFile('custData.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading data.json:', err);
+            res.status(500).json({ error: 'Internal Server Error' });
+            return;
+        }
 
-})
+        try {
+            const jsonArray = JSON.parse(data);
+            // Send the JSON array as the response
+            res.json(jsonArray);
+        } catch (error) {
+            console.error('Error parsing JSON:', error);
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+    });
+});
 
-app.get('/search', function (req, res) {
-    res.header("Content-Type", 'application/json');
-    const jsonData = require('./data.json');
-    res.send(JSON.stringify(jsonData));
-})
+app.post('/inventory', (req, res) => {
+    // Read the JSON array from data.json file
+    fs.readFile('inventoryData.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading data.json:', err);
+            res.status(500).json({ error: 'Internal Server Error' });
+            return;
+        }
+
+        try {
+            const jsonArray = JSON.parse(data);
+            // Send the JSON array as the response
+            res.json(jsonArray);
+        } catch (error) {
+            console.error('Error parsing JSON:', error);
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+    });
+});
+
+// Start the server and listen on the specified port
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
